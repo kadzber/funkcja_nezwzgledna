e03c7a39228408e409101cda06c88d67a7c8e86b
kacper <lockapo009@gmail.com>
2024-10-10 17:19:36 +0200

    made it prettier

--- java-script.js
+++ java-script.js
@@ -1,46 +1,39 @@
-function myFunc(WartoscBezwgledna){
+function myFunc(WartoscBezwgledna) {
+  let x = WartoscBezwgledna;
 
+  let liczba_bazwzgledna;
 
-let x = WartoscBezwgledna
+  if (x < 0) {
+    liczba_bazwzgledna = -1 * x;
+  } else {
+    liczba_bazwzgledna = x;
+  }
 
-let liczba_bazwzgledna;
+  let wynik;
 
-if (x<0){
-    liczba_bazwzgledna = -1 * x
+  if (x < 0) {
+    document.write(
+      "liczba bezwzgledna z" +
+        "‎ " +
+        x +
+        "‎ " +
+        "wynosi" +
+        "‎ " +
+        liczba_bazwzgledna
+    );
+  } else {
+    document.write("liczba jest dodatnia");
+  }
+
+  let element = document.getElementById("wynik");
+  element.textContent = wynik; // Uaktualnij zawartość elementu
 }
-
-else{ 
-    liczba_bazwzgledna = x
-} 
 
 let wynik;
 
-if (x<0){
-    document.write("liczba bezwzgledna z" + "‎ " + x  + "‎ " + "wynosi" + "‎ " + liczba_bazwzgledna)
-}
-else {
-    document.write("liczba jest dodatnia")
-}
-
-
-let element = document.getElementById("wynik");
-element.textContent = wynik; // Uaktualnij zawartość elementu
-}
-
-let wynik;
-
-document.getElementById("calculate_btn").addEventListener("click", function() {
-    // Pobierz wartość z pola wejściowego
-    let inputValue = document.getElementById("liczba_input").value;
-    // Zamień na liczbę
-    myFunc(Number(inputValue)); // Przekaż wartość jako liczbę do funkcji
+document.getElementById("calculate_btn").addEventListener("click", function () {
+  // Pobierz wartość z pola wejściowego
+  let inputValue = document.getElementById("liczba_input").value;
+  // Zamień na liczbę
+  myFunc(Number(inputValue)); // Przekaż wartość jako liczbę do funkcji
 });
-
-
-
-
-
-
-
-
-
